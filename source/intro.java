f79c7a45870bd585c987d59b2a1c8f2c2d2a36a0
angelcervant1 <angelcervant@hotmail.com>
2025-04-25 16:46:21 -0600

    add source folder

--- source/intro.java
+++ source/intro.java
@@ -9,9 +9,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class intro  extends World
 {
-    
+
     public intro()
-    
+
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1000, 600, 1);
@@ -25,13 +25,22 @@ public class intro  extends World
             addObject(new ground(),i*30+10,402);
         }
         for(int c=0;c<35;c++)
-            {
-                addObject(new ground(),c*30+10,528);
-            }
+        {
+            addObject(new ground(),c*30+10,528);
+        }
+        prepare();
     }
+
     public void act()
     {
         if(global.variables.j==0)
-        Greenfoot.setWorld(new World1());
+            Greenfoot.setWorld(new World1());
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
     }
 }
